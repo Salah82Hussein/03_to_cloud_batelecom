4048ee781e465156660ff578efb69e9e1d41d02f
Salah82Hussein <132533400+Salah82Hussein@users.noreply.github.com>
2023-05-05 08:07:02 +0300

    Update settings.py

--- batelecom/batelecom/settings.py
+++ batelecom/batelecom/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 import os
+import dj_database_url
 
 from pathlib import Path
 
@@ -21,13 +22,15 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-z%xp!4gykofj9krx$n7w1ro13vjitont7ayaruhh1krh_43wg+'
+#SECRET_KEY = 'django-insecure-z%xp!4gykofj9krx$n7w1ro13vjitont7ayaruhh1krh_43wg+'
+SECRET_KEY = os.environ.get('SECRET_KEY', default='your secret key')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+#DEBUG = True
+DEBUG = 'RENDER' not in os.environ
 
 ALLOWED_HOSTS = []
-
+RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')if RENDER_EXTERNAL_HOSTNAME:    ALLOWED_HOSTS.append(RENDER_EXTERNAL_HOSTNAME)
 
 # Application definition
 
@@ -49,6 +52,8 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
+    
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -84,7 +89,7 @@ WSGI_APPLICATION = 'batelecom.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
-
+'''
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
@@ -92,11 +97,16 @@ DATABASES = {
         'NAME': str(os.path.join(BASE_DIR, "db.sqlite3")),
     }
 }
-
+'''
+DATABASES = {
+    'default': dj_database_url.config(        
+        # Feel free to alter this value to suit your needs.        
+        default='postgresql://postgres:postgres@localhost:5432/batelecom',        
+        conn_max_age=600    )}
+        
 
 
 '''
-
 DATABASES = {
     "default": {
 #sql_server.pyodbc
@@ -177,6 +187,15 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+# Following settings only make sense on production and may break development environments.
+if not DEBUG:    # Tell Django to copy statics to the `staticfiles` directory
+    # in your application directory on Render.
+    STATIC_ROOT = os.path.join(BASE_DIR, '/b_tel_express/static/my_css')
+    # Turn on WhiteNoise storage backend that takes care of compressing static files
+    # and creating unique names for each version so they can safely be cached forever.
+    STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+    
+    
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 

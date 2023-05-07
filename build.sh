ed21d2c85ecf6bc9d964c6ac7fb2e28357e02732
Salah82Hussein <132533400+Salah82Hussein@users.noreply.github.com>
2023-05-07 08:27:55 +0300

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,17 +1,12 @@
 #!/usr/bin/env bash
-#!/usr/bin/env python
-
 # exit on error
 set -o errexit
 
-import os
-retval = os.getcwd()
-
 poetry install
 
-cd batelecom
+cd /batelecom/
 
 python manage.py collectstatic --no-input
 python manage.py migrate
 
-os.chdir( retval )
+cd /03_to_cloud_batelecom /
